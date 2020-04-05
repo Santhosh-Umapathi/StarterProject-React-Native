051bf92c9ea70df40ec5d38ae65f1445fabdfd28
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-04-05 09:49:46 +0200

    First Exercise Completed

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 
-const ComponentsScreen = () =>
-{
-    return <Text style={styles.textStyle}>ComponentsScreen</Text>;
-}
+const ComponentsScreen = () => {
+    const name = 'John'
+
+
+    return (
+      <View>
+        <Text style={styles.textStyle}>ComponentsScreen</Text>
+        <Text style={styles.subHeaderStyle}>My Name is {name}</Text>
+      </View>
+    );
+};
 
 const styles = StyleSheet.create({
     textStyle:
     {
         fontSize: 30,
     },
+    subHeaderStyle:
+    {
+        fontSize: 20
+    }
 });
 
 export default ComponentsScreen;
