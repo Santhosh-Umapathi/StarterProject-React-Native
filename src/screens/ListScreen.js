585a090e2faff32027cde53703ccf7e407a94054
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-04-05 10:41:34 +0200

    ListScreen exercise completed

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Text, FlatList, StyleSheet, View } from 'react-native';
+
+
+const ListScreen = () => {
+    
+    const Friends = [
+      { name: "John", age: 20 },
+      { name: "Jim", age: 30 },
+      { name: "Tim", age: 40 },
+      { name: "Javk", age: 50 },
+      { name: "Rav", age: 26 },
+      { name: "moi", age: 25 },
+      { name: "Hill", age: 40 },
+      { name: "Will", age: 24 }
+    ];
+
+
+
+
+    return (
+        <FlatList
+            //horizontal
+            //showsHorizontalScrollIndicator = {false}
+            data={Friends}
+            keyExtractor={key => key.name}
+            renderItem={({item}) => {
+                return (
+                    <Text style={styles.textStyle}>{item.name} - Age{item.age}</Text>
+                )
+            }}
+        />
+
+        
+    );
+}
+
+const styles = StyleSheet.create({
+    textStyle:
+    {
+        fontSize: 20,
+        marginVertical: 20
+    }
+})
+
+export default ListScreen;
