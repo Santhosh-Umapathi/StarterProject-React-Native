affc3b02548a23b0c153f7d088282f8598f58ced
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-04-06 11:49:31 +0200

    Counter screen exercise completed with Reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,12 +1,32 @@
-import React, {useState} from 'react';
+//Using Reducer
+
+import React, { useReducer } from 'react';
 import { View, Text, TextInput, StyleSheet, TouchableOpacity, Button  } from 'react-native';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "Increment":
+            return state.counter + action.payload < 0
+                ? state
+                : { ...state, counter: state.counter + action.payload };
+        
+        case "Decrement":
+            return state.counter + action.payload < 0
+              ? state
+              : { ...state, counter: state.counter + action.payload };
+        
+        default:
+            return state;
+    }
+};
+
+
 
 const CounterScreen = ({navigation}) =>
 {
 
-    const [counter, setCounter] = useState(0);
-
-
+    const [state, dispatch] = useReducer(reducer, { counter: 0 })
+    const { counter } = state;
        
     return (
         <View style={styles.containerView}>
@@ -14,17 +34,13 @@ const CounterScreen = ({navigation}) =>
             <Button
                 title="Increase"
                 onPress={() => {
-                    
-                        setCounter(counter + 1);
-                       
+                    dispatch({ type:'Increment' , payload: 1})
                 }}
             />
             <Button
                 title="Decrease"
                 onPress={() => {
-                    
-                      setCounter(counter - 1);
-                    
+                    dispatch({ type: "Decrement", payload: - 1 });
                 }}
             />
             <Text>{counter}</Text>
@@ -48,3 +64,54 @@ const styles = StyleSheet.create({
 });
 
 export default CounterScreen;
+
+//--------------------------------------------------------
+// Using State Mangement
+
+
+// import React, { useState } from "react";
+// import {
+//   View,
+//   Text,
+//   TextInput,
+//   StyleSheet,
+//   TouchableOpacity,
+//   Button,
+// } from "react-native";
+
+// const CounterScreen = ({ navigation }) => {
+//   const [counter, setCounter] = useState(0);
+
+//   return (
+//     <View style={styles.containerView}>
+//       <Text style={styles.text}>CounterScreen</Text>
+//       <Button
+//         title="Increase"
+//         onPress={() => {
+//           setCounter(counter + 1);
+//         }}
+//       />
+//       <Button
+//         title="Decrease"
+//         onPress={() => {
+//           setCounter(counter - 1);
+//         }}
+//       />
+//       <Text>{counter}</Text>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   containerView: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   text: {
+//     fontSize: 30,
+//   },
+// });
+
+// export default CounterScreen;
